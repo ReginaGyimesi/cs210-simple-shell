571390949549a09c5101371fc53454b3cfcb93f0
regina <regina.gyimesi.2019@uni.strath.ac.uk>
2021-01-27 20:08:36 +0100

    testing

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Regina Gyimesi on 27/01/2021.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,13 @@
+//
+// Created by Regina Gyimesi on 27/01/2021.
+//
+
+#ifndef UNTITLED_TEST_H
+#define UNTITLED_TEST_H
+
+
+class test {
+};
+
+
+#endif //UNTITLED_TEST_H
