d5fd84e84f642c8217d32c9caf7c6072508fb11d
balazs <balazs.szalay.2019@uni.strath.ac.uk>
2021-02-01 14:15:25 +0100

    added command application function

--- functions/applycommand.h
+++ functions/applycommand.h
@@ -1,6 +1,6 @@
 /*
 // taking an array of tokenized inputs
-// executing if its a valid command
+// executing a command
 // Created by Balázs on 2021. 02. 01..
 */
 
@@ -8,17 +8,17 @@
 #define CS210_SIMPLE_SHELL_APPLYCOMMAND_H
 
 #endif //CS210_SIMPLE_SHELL_APPLYCOMMAND_H
-int apply_command(char** tokens){
-    int i=0;
-    if(tokens[0]==NULL) //if the input is null then nothing to apply hence return 1
-    {
-        return 1;
-    }
-    switch (tokens[0]) //if not see if its one of our own functions so far only exit
-    {
-        case "exit"||EOF:
-            return 0;
 
+int apply_command(char** tokens)
+{
+    if (tokens[0]==NULL)
+    {   //if the first input is null we just return -1 cuz its gonna be an error
+        return -1;
     }
-
-}
+    char * inbuilt[]={"exit"}; //an array where we store our non-linux commands
+                               // so far only exit but will grow later
+    for (int i = 0; i < 1; ++i) {
+        if (strcmp(tokens[0],inbuilt[i])==0) //checking if the input is an inbuilt function and returning the
+            return i;                        // the index if it
+    }
+    return 1;       //here we will fork but currently returns 1 if its not null and not exit
