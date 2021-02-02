b9121e08cb6b9df87c69d5ca7fc8df6796080e51
Balazs Szalay SE2019 <balazs.szalay.2019@uni.strath.ac.uk>
2021-02-02 13:35:16 +0000

    Update applycommand.h

--- functions/applycommand.h
+++ functions/applycommand.h
@@ -12,10 +12,10 @@
 #endif //CS210_SIMPLE_SHELL_APPLYCOMMAND_H
 
 int apply_command(char** tokens) {
-    if (tokens == NULL || tokens[0] == NULL) {   //if the first input is null we just return -1 cuz its gonna be an error
-        return -1;
+    if (tokens == NULL || tokens[0] == NULL) {   //Should return 0 to close the input feed and exit the loop
+        return 0;
     }
-    char *inbuilt[] = {"exit", "^Z", "^D"}; //an array where we store our non-linux commands
+    char *inbuilt[] = {"exit",}; //an array where we store our non-linux commands
     // so far only exit but will grow later
     for (int i = 0; i < 3; ++i) {
         if (strcmp(tokens[0], inbuilt[i]) == 0) //checking if the input is an inbuilt function and returning the
