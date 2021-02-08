57ec6697d75a7b994ad45843527c71e28300ecac
Zoltan Kiss CS2019 <zoltan.kiss.2019@uni.strath.ac.uk>
2021-02-08 19:21:24 +0000

    Updated with Stage 2

--- main/main.c
+++ main/main.c
@@ -2,39 +2,26 @@
 // Created by zkiss on 27/01/2021.
 //
 
-#include <main.h>
+#include "main.h"
 
 int main()
 {
-    int run_testing_methods = FALSE;
-    if(run_testing_methods){
-        testing(run_testing_methods);
-    }
-    else{
-        char *line;
-        char **args;
-        int status;
+    char *line;
+    char **args;
+    int status;
 
-        do {
+    do {
 
-            print_prompt();
-            line = read_input();
+        print_prompt();
+        line = read_input();
 
-            args = tokenise(line);
+        args = tokenise(line);
 
-            status = apply_command(args);
+        status = apply_command(args);
 
-            free(line);
-            free(args);
-        } while (status);
-    }
+        free(line);
+        free(args);
+    } while (status);
 
     return 0;
-}
-
-void testing(int run_every_test){
-    if(run_every_test){
-        run_tests();
-    }
-
 }
