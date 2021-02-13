0e4a75732922e4aad80230d5b3878ec9b99bb3ba
Zoltan Kiss CS2019 <zoltan.kiss.2019@uni.strath.ac.uk>
2021-02-13 21:05:01 +0000

    Updated change_directory

--- main/inbuilt.h
+++ main/inbuilt.h
@@ -16,31 +16,38 @@
 /*
  * Function to change directory
  */
-int change_directory(char** tokens, char** history){
+int change_directory(char** tokens) {
 
-    // initial error checking
-    if(tokens == NULL || *tokens == NULL || tokens[0] == NULL){
-        fprintf(stderr, "NULL pointer error in cd command");
+    // initial error checks
+    if (tokens == NULL || *tokens == NULL || tokens[0] == NULL) {
+        perror("NULL pointer error in cd command\n");
         return ERROR;
     }
 
-    if(tokens[1] == NULL){                      // no parameter were given, set to user's home directory
-        if(chdir(getenv("HOME"))!=0){
-            perror("Failed to change HOME");
+    if (tokens[2] != NULL) {
+        fprintf(stderr, "Too many arguments. Use case: cd [path/to/directory]\n");
+        return ERROR;
+    }
+
+    if (tokens[1] == NULL) { // no parameter were given, set to user's home directory
+        if (chdir(getenv("HOME")) != 0) {
+            perror("Failed to change HOME, maybe it does not exist?");
             return ERROR;
         }
         return TRUE;
-    }
-
-    else{                                       // some parameter were given, execute cd with that param
-        if(chdir(tokens[1]) != 0){        // disregarding other params, only using the next param after cd
-            perror("Failed to change");
+    } else {               // some parameter were given, execute cd with that param
+        if (chdir(tokens[1]) != 0) {  // disregarding other params, only using the next param after cd
+            char *s = malloc(sizeof(char *) * 100);
+            strcpy(s, "Failed to change to ");
+            strcat(s, tokens[1]);
+            strcat(s, " Use case: cd [path/to/directory]");
+            perror(s);
+            free(s);
             return ERROR;
         }
     }
 
     return TRUE;
-
 }
 
 int exit1()
