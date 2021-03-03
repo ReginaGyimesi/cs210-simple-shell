05412293488b92d56d4e1530ec179c459b04e4ad
Zoltan Kiss CS2019 <zoltan.kiss.2019@uni.strath.ac.uk>
2021-03-03 21:34:11 +0000

    Comments for Stage 9 part

--- main/applycommand.h
+++ main/applycommand.h
@@ -42,35 +42,93 @@ int apply_command(char** tokens, char** history,int* front, int* rear,AList alia
     }
     else
     {
-        int one_of_history_or_alias = TRUE;
-        int limit = 0;
-        while(one_of_history_or_alias && limit < 20){
+        // This is where or main command repetition happening
+        int one_of_history_or_alias = TRUE;     // If this is false, that means the latest command wasnt a history or a alias command
+        int limit = 0;                          // Our maximum recurring limit
+        while(one_of_history_or_alias && limit < 20){       // This is a O(n^2) function, where n is the length of the command
 
-            while(tokens != NULL && limit < 20 && (*tokens)[0] == '!'){                        // check if history command
+            /*
+             * We are first checking for a history command.
+             * So a history command can either return a new set of tokens or NULL
+             * For example:
+             *      !5 -> echo "Hello World!": in this case this whole loop would run only
+             *                                 once, because `echo` is a Unix command and
+             *                                 would exit by the one_of_history_or_alias = FALSE
+             *                                 (this while loop will run once as well).
+             *      !5 -> history:             same as before.
+             *      !5 -> !9:                  a case that never happens, but if it does, this tiny
+             *                                 while loop runs twice, and calls whatever !9 stands for.
+             *      !5 -> alias:               whole loop and this loop runs once, exits as NULL after
+             *                                 check_alias()
+             *      !5 -> getpath:             (in-built command, same as a Unix command) whole loop and this loop runs twice, exits as
+             *                                 one_of_history_or_alias = FALSE
+             *      !5 -> iamgarbage:          this would return itself, and exits by one_of_history_or_alias = FALSE
+             */
+            while(tokens != NULL && limit < 20 && (*tokens)[0] == '!'){
                 tokens = check_history_type(tokens, history, front, rear);
+            }
 
-            }   // we need else if, why? because the input "!clear" will return NULL, and it will cause errors
-
+            /*
+             * Check if the history checker returned NULL (most possible case):
+             *      !5 -> !clear
+             */
             if(tokens == NULL){
                 return TRUE;
             }
 
+            /*
+             * After we made sure that the token is not a history command anymore
+             * We move on to the aliases. Here we check, if this command is a
+             * alias setting command (but not the aliased command)
+             * Alias setting commands always return NULL so here that is the only exit
+             * condition. I have no idea, why did I put a while loop here, because we cannot
+             * recursively check for the tokens, because every return is NULL.
+             */
             while(tokens != NULL && limit < 20 && (strcmp(tokens[0],"alias") == 0 || strcmp(tokens[0],"unalias") == 0)){
                 tokens = check_alias(tokens,aliases);
-
             }
 
+            /*
+             * If it was an alias setting command, then we return
+             */
             if(tokens == NULL){
                 return TRUE;
             }
 
+            /*
+             * This is where we check, that the command may be some aliased word.
+             * So if we are here, we might establish, that the command is not a history invocation
+             * nor an alias setting command. So it from now it can be 4 thing:
+             *  - garbage
+             *  - an aliased word
+             *  - either an in-built or Unix command
+             *  - (or it is actually a history or alias setting command,
+             *    so we have to start the whole loop again)
+             *
+             * get_key_from_tokens() will return new tokens (the aliased command)
+             * or the same tokens that were inputted. This also sets the
+             * one_of_history_or_alias boolean to FALSE, if the tokens are NULL or no
+             * such aliased command was found.
+             */
             tokens = get_key_from_tokens(tokens, aliases, &one_of_history_or_alias);
 
+            /*
+             * If somehow the tokens becomes NULL, we just quit the loop.
+             */
             if(tokens == NULL){
                 one_of_history_or_alias = FALSE;
             }
 
-            limit++;
+            limit++;        // We increase our limit, to dont stuck in a infinite loop.
+
+            /*
+             * And we start the whole loop again. Either because the original tokens were
+             * overwritten by something new or none of the methods here set a exit condition.
+             *
+             * If there is a exit condition, we move on with our life to either execute an in-built
+             * command or a Unix-command (or neither), with the new set of tokens that were overwritten by
+             * one of the methods.
+             */
         }
 
         if(tokens!=NULL){
